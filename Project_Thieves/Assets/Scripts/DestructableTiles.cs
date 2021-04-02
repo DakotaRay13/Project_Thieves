3af2d413e51bc65cadb1f755ffd4f9559b869abe
DakotaRay13 <dakotarayonps4@gmail.com>
2021-04-01 23:22:56 -0500

    Destructible Tiles Added

---
+++ Project_Thieves/Assets/Scripts/DestructableTiles.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DestructableTiles : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<AlexAttack>())
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
